9e849977018b06a8f2dc6819a3b4c5b41190e887
Jhey <macarubbo.david@gmail.com>
2018-06-09 19:32:35 +0800

    Added Google geolocation API key configuration

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const request = require('request')
+const keys = require('./config/key')
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=mall%20of%20asia%202',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=mall%20of%20asia%202&key=${keys.GOOGLE_GEOLOCATION_API_KEY}`,
   json: true
 }, (err, response, body) => {
   console.log(body)
